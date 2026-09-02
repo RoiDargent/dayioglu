6911b10a32531247d64b3a377bc643bb01f55da7
Zeynel İTKÜ <itkuzeynel@gmail.com>
2026-09-02 12:28:04 +0300

    vercel için deneme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         
         {/* Logoya tıklayınca da en üste kaysın diye href="#baslangic" yapıldı */}
         <a href="#baslangic" className="text-xl font-black font-montserrat tracking-widest drop-shadow-md">
-          DAYIOĞLU İNŞAAT
+          DAYIaOĞLU İNŞAAT
         </a>
         
         {/* Masaüstü Menü (Mobilde gizlenir - md:flex) */}
